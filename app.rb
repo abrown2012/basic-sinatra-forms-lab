8d4b3a65cdd1da902302a4443de7cbbace8734d7
Agnieszka Brown <abrown2012@icloud.com>
2021-01-17 21:52:56 -0500

    commit tree fe9c8fd86ae814355dbe826a6c4a8bb5af705b99

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do 
+        erb :newteam
+    end
 
-
+    post '/team' do 
+        "Team Name: #{params[:name]}/n Coach: #{params[:coach]}/n Point Guard: #{params[:pg]}/n Shooting Guard: #{params[:sg]}/n Small Forward: #{params[:sf]}/n Power Forward: #{params[:pf]}/n Center: #{params[:c]}"
+    end 
 end
